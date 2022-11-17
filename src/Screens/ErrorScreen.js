5f4849729119113c802b3b89a5f2d2166cf4116f
Harleeyah <92531245+Harleeyah@users.noreply.github.com>
2022-11-17 09:29:20 +0100

    Update ErrorScreen.js

--- src/Screens/ErrorScreen.js
+++ src/Screens/ErrorScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Person } from '../Components/Person'
+import Person  from '../Components/Person'
 import { ClickCounter } from '../Components/ClickCounter'
 import { ErrorBoundary } from "react-error-boundary";
 import { Fallback } from '../Components/Fallback'
